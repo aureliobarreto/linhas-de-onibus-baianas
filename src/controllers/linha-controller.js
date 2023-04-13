edef428d4438ccd86dd6bf767bad4a19e916af93
amandassa <amandassa504@gmail.com>
2023-04-13 10:29:22 -0300

    Update routes

--- src/controllers/linha-controller.js
+++ src/controllers/linha-controller.js
@@ -28,7 +28,6 @@ exports.post = (req, res, next) => {
 };
 
  exports.buscarLinhaPorNome = async (req, res, next) => {
-    console.log(req.body.nome)
     await Linha.find({ nome: new RegExp(req.body.nome, 'i')}).exec().then(data => {
         if(data != null){
             res.status(200).send(data)
@@ -42,4 +41,50 @@ exports.post = (req, res, next) => {
         res.status(400).send(e)
     })        
     
+}
+
+exports.buscarLinhaPorParada = async (req, res, next) => {
+    // select all from linhas where paradas contains
+    console.log(req.body)
+    await Linha.find({paradas: req.body.nome}).exec().then(data => {
+    // await Linha.find({paradas: new RegExp(req.body.nome, 'i')}).exec().then(data => {
+        if(data != null){
+            res.status(200).send(data)
+        }else{
+            res.status(404).send({
+                message: "linha não encontrada"
+            })
+        }
+        
+    }).catch(e => {
+        res.status(400).send(e)
+    })    
+}
+exports.buscarLinhaPorParadaEAgenda = async (req, res, next) => {
+    await Linha.find({paradas: new RegExp(req.body.nome, 'i')}).find({horariosOrigem: req.body.hora}).exec().then(data => {
+        if(data != null){
+            res.status(200).send(data)
+        }else{
+            res.status(404).send({
+                message: "linha não encontrada"
+            })
+        }
+        
+    }).catch(e => {
+        res.status(400).send(e)
+    })        
+}
+exports.buscarLinhaPorTrecho = async (req, res, next) => {
+    await Linha.find({paradas: {$all: [new RegExp(req.body.origem, 'i'), new RegExp(req.body.destino, 'i')]}}).exec().then(data => {
+        if(data != null){
+            res.status(200).send(data)
+        }else{
+            res.status(404).send({
+                message: "linha não encontrada"
+            })
+        }
+        
+    }).catch(e => {
+        res.status(400).send(e)
+    })        
 }
--- src/routes/linha-routes.js
+++ src/routes/linha-routes.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/linha-controller');
 router.post('/', controller.post);
 router.get('/', controller.get);
 router.get('/buscarLinhaPorNome', controller.buscarLinhaPorNome);
+router.get('/buscarLinhaPorParada', controller.buscarLinhaPorParada);
+router.get('/buscarLinhaPorTrecho', controller.buscarLinhaPorTrecho);
 
 module.exports = router;
