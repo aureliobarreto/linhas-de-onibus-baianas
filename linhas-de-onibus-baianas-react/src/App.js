2c1c6d89470292bd4b72506c44f6818ee18319e8
Aurelio <aurelionadjabarreto@gmail.com>
2023-05-08 15:33:09 -0300

    projeto react

---
+++ linhas-de-onibus-baianas-react/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Linhas de Ônibus Baianas</h1>
+    </div>
+  );
+}
+
+export default App;
