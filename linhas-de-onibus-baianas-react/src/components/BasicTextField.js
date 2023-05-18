2d36dd3bb56557e7422daedc0eba49de5139ce80
Aurelio <aurelionadjabarreto@gmail.com>
2023-05-18 16:47:03 -0300

    Update BasicTextField.js

--- linhas-de-onibus-baianas-react/src/components/BasicTextField.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-
-export default function BasicTextField({ title }) {
-  return (
-      <TextField variant="outlined" label={title} />
-  );
-}
