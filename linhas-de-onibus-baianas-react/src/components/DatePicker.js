dc8e2c1dae8bea897849bc97397f3c89ddeaf9da
amandassa <amandassa504@gmail.com>
2023-05-19 00:18:30 -0300

    update

--- linhas-de-onibus-baianas-react/src/components/DatePicker.js
+++ linhas-de-onibus-baianas-react/src/components/DatePicker.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 
 export default function BasicDatePicker() {
   const [value, setValue] = React.useState(dayjs());
   return (
-    <LocalizationProvider  dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker']}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker value={value} onChange={(newValue) => setValue(newValue)} />
-      </DemoContainer>
     </LocalizationProvider>
   );
 }
--- linhas-de-onibus-baianas-react/src/pages/Home.js
+++ linhas-de-onibus-baianas-react/src/pages/Home.js
@@ -1,42 +1,46 @@
+import Checkbox from '@mui/material/Checkbox';
+import CardActions from '@mui/material/CardActions';import FormControlLabel from '@mui/material/FormControlLabel';
 import BasicDatePicker from "../components/DatePicker"
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-//import BasicButton from "../components/BasicButton";
 import Button from '@mui/material/Button';
 import MyMap from "../components/MyMap";
-import ButttonAppBar from "../components/ButtonAppBar";
+import ButtonAppBar from "../components/ButtonAppBar";
 import React from "react";
 import '../pages/Home.css'
+import { Card } from '@mui/material';
 
 function Home() {
     return (
-        
         <div>
-            <ButttonAppBar />
-        <Box
-            justifyContent="center"
-            component="div"
-            sx={{
-                display: 'flex',
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            >
-            <BasicDatePicker  />
-            <TextField id="outlined-basic" label="Oriegm" variant="outlined" />
-            <TextField id="outlined-basic" label="Destino" variant="outlined" />
-            <Button variant="contained" size="small" color="primary" >Buscar</Button>
-        </Box>           
-            
-            {/* Abaixo terá uma div com dimensões quadradas para receber o mapa */}
-                <div className="map">
-                    <h1>Mapa</h1>  
-                    <MyMap className="map" />
-                </div>
+            <ButtonAppBar />
+            <Box
+                justifyContent="center"
+                component="div"
+                sx={{
+                    display: 'flex',
+                    '& > :not(style)': { m: 3, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <BasicDatePicker  />
+                <TextField id="outlined-basic" label="Origem" variant="outlined" />
+                <TextField id="outlined-basic" label="Destino" variant="outlined" />
+                <Button variant="contained" size="small" color="primary" >Buscar</Button>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Todos os dias" />
+            </Box>           
                 
-           
-           
+            {/* Abaixo terá uma div com dimensões quadradas para receber o mapa */}
+            <Card className="map">
+                <h3>Mapa</h3>  
+                {/* <CardActions>
+                <Button size="small" color="primary">
+                Ver
+                </Button>
+                </CardActions> */}
+                <MyMap className="map" />
+            </Card>
         </div>
     )
 }
